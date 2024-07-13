58d76fc4e68b45268a89b2c0f08c281e49cd0415
Lucas Santos <lucsantosdev@gmail.com>
2024-07-12 22:25:41 -0300

    mongoose installation and database connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { PORT } from "./config.js"
+import mongoose from "mongoose"
 
 const app = express()
 
@@ -8,6 +9,15 @@ app.get('/', (req, res) => {
     return res.status(234).send('Hello World')
 })
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-})
+// database connection
+mongoose
+    .connect('mongodb://localhost:27017/product-list')
+    .then(() => {
+        console.log('App connected to database')
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
