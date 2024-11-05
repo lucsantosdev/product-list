31824feca9723691ff1d3495e9eb41ba0d1f3681
Lucas Santos <lucsantosdev@gmail.com>
2024-11-05 11:38:32 -0300

    handling with CORS policy

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,24 @@ import { PORT } from "./config.js"
 import mongoose from "mongoose"
 import { Product } from "./models/productModel.js"
 import productRoute from './routes/productsRoute.js'
+import cors from 'cors'
 
 const app = express()
 
 // middleware for parsing request body
 app.use(express.json())
+
+// middleware for handling CROS POLICY
+// option 1: allow all origins with default  of cors
+app.use(cors())
+// option 2: allow custom origins
+app.use(
+    cors({
+        origin: 'htpps://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/', (req, res) => {
     console.log(req)
