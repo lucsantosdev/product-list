25976ce0f143586be6217d6175275935947d2fbb
Lucas Santos <lucsantosdev@gmail.com>
2024-12-08 12:25:49 -0300

    SPA and Add React Router DOM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import CreateProducts from "./pages/CreateProducts";
+import ShowProduct from "./pages/ShowProduct";
+import EditProduct from "./pages/EditProduct";
+import DeleteProduct from "./pages/DeleteProduct";
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/products/create' element={<CreateProducts />} />
+      <Route path='/products/details/:id' element={<ShowProduct />} />
+      <Route path='/products/edit/:id' element={<EditProduct />} />
+      <Route path='/products/delete/:id' element={<DeleteProduct />} />
+    </Routes>
   )
 }
 
