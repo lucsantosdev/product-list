aa90633b480d9652c5c4b145e12cfe6b04b84477
Lucas Santos <lucsantosdev@gmail.com>
2024-12-18 18:41:47 -0300

    Showing Book Details in React

--- frontend/src/pages/ShowProduct.jsx
+++ frontend/src/pages/ShowProduct.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const ShowProduct = () => {
+    const [product, setProduct] = useState({})
+    const [loading, setLoading] = useState(false)
+    const { id } = useParams()
+
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .get(`http://localhost:5555/products/${id}`)
+            .then((response) => {
+                setProduct(response.data)
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.log(error)
+                setLoading(false)
+            })
+    }, [])
     return (
-        <div>ShowProduct</div>
+        <div className="p-4">
+            <BackButton />
+            <h1 className="text-3xl my-4">Show Book</h1>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Id</span>
+                        <span>{product._id}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Name</span>
+                        <span>{product.type}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Description</span>
+                        <span>{product.description}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Type</span>
+                        <span>{product.type}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Create Time</span>
+                        <span>{new Date(product.createdAt).toString()}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+                        <span>{new Date(product.updatedAt).toString()}</span>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
